bd37f58e4e411e1bd95d40d55aefe414f560accd
Pedro Bianchi <pedroedm@gmail.com>
2023-02-05 11:57:11 -0300

    Implements generation of simple reports

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,19 @@
-class SimpleReport:
-    pass
+from collections import Counter
+
+
+class SimpleReport():
+    @staticmethod
+    def generate(inventory: list):
+        list = []
+
+        oldest = min(product["data_de_fabricacao"] for product in inventory)
+        expire_next = min(product["data_de_validade"] for product in inventory)
+
+        for item in inventory:
+            list.append(item["nome_da_empresa"])
+
+        return (
+            f"Data de fabricação mais antiga: {oldest}\n"
+            f"Data de validade mais próxima: {expire_next}\n"
+            f"Empresa com mais produtos: {Counter(list).most_common()[0][0]}"
+            )
