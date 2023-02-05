e56be3dcb1af791ee8af17e440fb9969fa1a5b3f
Pedro Bianchi <pedroedm@gmail.com>
2023-02-05 15:53:34 -0300

    Generate a report from a json file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,19 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @classmethod
     def import_data(cls, file_path, report_type):
         with open(file_path) as file:
-            reader = csv.DictReader(file)
-            data = list(reader)
+            if file_path.endswith(".csv"):
+                reader = csv.DictReader(file)
+                data = list(reader)
+
+            elif file_path.endswith(".json"):
+                data = json.load(file)
 
         if report_type == "simples":
             return SimpleReport.generate(data)
