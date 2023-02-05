470316d27fbc57173c56d732ddac6db375d670e6
Pedro Bianchi <pedroedm@gmail.com>
2023-02-05 12:36:05 -0300

    Implements generation of complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    def generate(inventory):
+        simple_report = SimpleReport.generate(inventory)
+        comp = Counter(product["nome_da_empresa"] for product in inventory)
+        response = "Produtos estocados por empresa:\n"
+
+        for name, quantity in comp.items():
+            response += (f"- {name}: {quantity}\n")
+
+        return (
+            f"{simple_report}\n"
+            f"{response}\n"
+        )
